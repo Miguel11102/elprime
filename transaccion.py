eb4d1f98a6fcfa6e05c6a1164c4d4999261a607e
Miguel11102 <160886700+Miguel11102@users.noreply.github.com>
2024-05-09 09:47:52 -0500

    Update transaccion.py

--- transaccion.py
+++ transaccion.py
@@ -1,22 +1,20 @@
 class transaccion:
+    TIPO = {
+        1:'Venta',
+        2:'ABASTECIMIENTO'
+    }
 
-    """----------------------------------
-    #metodos
-    ----------------------------------"""
+    def __init__(self, tipo, cantidad, fecha):
 
-    def __init__(self, pFecha, pTipo, pCantidad):
-        self.__fecha = pFecha
-        self.__tipo = (self.TIPO.abast, self.TIPO.venta)[pTipo]
-        self.__cantidad = pCantidad
-
+        self.__tipo = self.TIPO['1'] if tipo == 1 else self.TIPO['2']
+        self.__cantidad = cantidad
+        self.__fecha = fecha
+    
     def getTipo(self):
         return self.__tipo
     
-    def getFecha(self):
-        return self.__fecha
-    
-    def getCantidad (self):
-        return self.__cantidad
-    
+    def setTipo(self, tipo):
+        self.__tipo = self.TIPO['1'] if tipo == 1 else self.TIPO['2']
+
     
     
